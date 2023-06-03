9d8ba89a8974b5677e3c1a96994fed18d06999bd
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-06-03 17:54:18 -0300

    adicionado listagem de pesquisadores

--- mysite/appPesquisa/urls.py
+++ mysite/appPesquisa/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /appPesquisa/
     path("", views.index, name="index"),
+    # ex: /appPesquisa/pesquisadores/
+    path("pesquisadores/", views.listarPesquisadores, name="listarPesquisadores"),
+    # ex: /appPesquisa/5/
+    path("pesquisador/<int:pesquisador_id>/", views.pesquisadorDetail, name="pesquisadorDetail"),
+    # ex: /appPesquisa/5/pesquisadorprojetos/
+    path("pesquisador/<int:pesquisador_id>/projetos/", views.pesquisadorProjetos, name="pesquisadorProjetos"),
+    # ex: /appPesquisa/5/pesquisadorresultados/
+    path("pesquisador/<int:pesquisador_id>/resultados/", views.pesquisadorResultados, name="pesquisadorResultados"),
 ]
--- mysite/appPesquisa/views.py
+++ mysite/appPesquisa/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Pesquisador
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the pesquisa index.")
+    pesquisadores = Pesquisador.objects.order_by("nome")
+    context = {
+        "pesquisadores": pesquisadores,
+    }
+    return render(request, "pesquisador/index.html", context)
+
+def listarPesquisadores(request):
+    return HttpResponse("Você está procurando uma lista de pesquisadores")
+
+def pesquisadorDetail(request, pesquisador_id):
+    return HttpResponse("Você está procurando por %s." % pesquisador_id)
+
+def pesquisadorProjetos(request, pesquisador_id):
+    response = "Você está procurando pelos projetos de %s."
+    return HttpResponse(response % pesquisador_id)
+
+def pesquisadorResultados(request, pesquisador_id):
+    response = "Você está procurando pelos resultados de %s."
+    return HttpResponse(response % pesquisador_id)
