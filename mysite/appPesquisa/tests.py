218b5d1f26f4143156ec75da03e0c5bb8fa0120e
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-06-06 00:16:53 -0300

    atualizado testes automatizados

--- mysite/appPesquisa/tests.py
+++ mysite/appPesquisa/tests.py
@@ -49,7 +49,7 @@ class PesquisadorListFilterTest(TestCase):
         # Criando alguns pesquisadores de exemplo
         Pesquisador.objects.create(nome='João', tipo='aluno', area_atuacao='Biologia')
         Pesquisador.objects.create(nome='Maria', tipo='professor',  area_atuacao='Química')
-        Pesquisador.objects.create(nome='Pedro', tipo='funcionario',  area_atuacao='Física')
+        Pesquisador.objects.create(nome='Pedro', tipo='professor',  area_atuacao='Física')
 
     def test_pesquisador_filtro_por_nome(self):
         # Realiza uma pesquisa por nome
@@ -59,27 +59,47 @@ class PesquisadorListFilterTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # Verifica se a pesquisa retornou apenas o pesquisador correto
-        self.assertEqual(len(response.context['pesquisadores']), 1)
         self.assertEqual(response.context['pesquisadores'][0].nome, 'João')
         self.assertEqual(response.context['pesquisadores'][0].tipo, 'aluno')
         self.assertEqual(response.context['pesquisadores'][0].area_atuacao, 'Biologia')
 
-    def test_pesquisador_filtro_por_area_atuacao(self):
+    def test_pesquisador_filtro_por_tipo(self):
         # Realiza uma pesquisa por nome
-        response = self.client.get(reverse('appPesquisa:pesquisador_list'), {'area_atuacao': 'Biologia'})
+        response = self.client.get(reverse('appPesquisa:pesquisador_list'), {'tipo': 'professor'})
 
         # Verifica se a resposta é 200 (sucesso)
         self.assertEqual(response.status_code, 200)
 
         # Verifica se a pesquisa retornou apenas o pesquisador correto
-        self.assertEqual(len(response.context['pesquisadores']), 1)
-        self.assertEqual(response.context['pesquisadores'][0].nome, 'João')
-        self.assertEqual(response.context['pesquisadores'][0].tipo, 'aluno')
-        self.assertEqual(response.context['pesquisadores'][0].area_atuacao, 'Biologia')
+        self.assertEqual(response.context['pesquisadores'][0].nome, 'Maria')
+        self.assertEqual(response.context['pesquisadores'][0].tipo, 'professor')
+        self.assertEqual(response.context['pesquisadores'][0].area_atuacao, 'Química')
+
+    def test_pesquisador_filtro_por_area_atuacao(self):
+        # Realiza uma pesquisa por nome
+        response = self.client.get(reverse('appPesquisa:pesquisador_list'), {'area_atuacao': 'Física'})
+
+        # Verifica se a resposta é 200 (sucesso)
+        self.assertEqual(response.status_code, 200)
+
+        # Verifica se a pesquisa retornou apenas o pesquisador correto
+        self.assertEqual(response.context['pesquisadores'][0].nome, 'Pedro')
+        self.assertEqual(response.context['pesquisadores'][0].tipo, 'professor')
+        self.assertEqual(response.context['pesquisadores'][0].area_atuacao, 'Física')
 
     def test_pesquisador_filtro_por_nome_sem_resultados(self):
         # Realiza uma pesquisa por nome que não retorna resultados
         response = self.client.get(reverse('appPesquisa:pesquisador_list'), {'nome': 'Carlos'})
+
+        # Verifica se a resposta é 200 (sucesso)
+        self.assertEqual(response.status_code, 200)
+
+        # Verifica se a pesquisa não retornou nenhum resultado
+        self.assertEqual(len(response.context['pesquisadores']), 0)
+
+    def test_pesquisador_filtro_por_tipo_sem_resultados(self):
+        # Realiza uma pesquisa por nome que não retorna resultados
+        response = self.client.get(reverse('appPesquisa:pesquisador_list'), {'tipo': 'funcionario'})
 
         # Verifica se a resposta é 200 (sucesso)
         self.assertEqual(response.status_code, 200)
