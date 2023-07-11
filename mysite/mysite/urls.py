1983cefc7d637005eb3db3bc385c2f22ca68521b
VIEZZER <33733608+viezzer@users.noreply.github.com>
2023-07-11 20:16:17 -0300

    atualização interface gráfica

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("appPesquisa/", include("appPesquisa.urls")),
+    path("", include("appPesquisa.urls")),
     path('admin/', admin.site.urls),
 ]
